69041c7b7d90ca15856e9c26263d9042a290790e
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2025-07-31 21:46:22 -0300

    Resilience4j Retry Implementation

---
+++ MicroservicesWithFeign/book-service/src/main/java/com/felipe/arruda/book_service/controller/FooBarController.java
@@ -0,0 +1,28 @@
+package com.felipe.arruda.book_service.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+    @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar")
+    public String fooBar() {
+        logger.info("Request to foo-bar is received!!");
+
+        ResponseEntity<String> response = new RestTemplate()
+                .getForEntity("http://localhost:8000/book-service", String.class);
+        return response.getBody();
+    }
+
+}
